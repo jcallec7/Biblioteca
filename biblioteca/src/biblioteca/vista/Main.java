4e9a774bc5d36fdb54ca9a94fa6b9e3192ce5967
pedro <pedro@LAPTOP-ILA641ID>
2019-11-10 19:45:07 -0500

    Conexion hecha

--- biblioteca/src/biblioteca/vista/Main.java
+++ biblioteca/src/biblioteca/vista/Main.java
@@ -10,21 +10,26 @@ public class Main {
 	public static void main(String[] args) {
 
 		Operacion con = new Operacion();
-		Conexion co= new Conexion();
-		 co.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
-	        co.setUsername("hr");
-	        co.setPassword("hr");
-	        
-	        co.Conectar();
-	        
-	        if(co.getConexion() != null){
-	        	
-	            System.out.println("La base se ha conectado exitosamente");
-	    		VentanaPrincipal vp = new VentanaPrincipal(con, co);
-	    		vp.setVisible(true);
-	    		
-	        }
-		
+
+		/*
+		 * Conexion co = new Conexion();
+		 * co.setUrl("jdbc:oracle:thin:@localhost:1521:XE"); co.setUsername("hr");
+		 * co.setPassword("hr");
+		 * 
+		 * co.Conectar();
+		 * 
+		 * if (co.getConexion() != null) {
+		 * 
+		 * System.out.println("La base se ha conectado exitosamente"); VentanaPrincipal
+		 * vp = new VentanaPrincipal(con, co); vp.setVisible(true);
+		 * 
+		 * }
+		 */
+
+		Conexion co = new Conexion();
+
+		System.out.println("La base se ha conectado exitosamente"); VentanaPrincipal
+		vp = new VentanaPrincipal(con, co); vp.setVisible(true);
 		
 	}
 
