01d6c673fc126c483c11c733054e28979ca7bee2
jcall <jcall@DELL-JOSE>
2019-12-01 17:39:58 -0500

    Actualizacion de clase persona

---
+++ biblioteca/src/biblioteca/modelo/Cliente.java
@@ -0,0 +1,18 @@
+package biblioteca.modelo;
+import java.sql.Date;
+
+import biblioteca.modelo.Persona;
+
+public class Cliente extends Persona {
+	
+	public Cliente(int id, String nombre, String apellido, Boolean genero, Date fechaNac) {
+		super(id, nombre, apellido, genero, fechaNac);
+		// TODO Auto-generated constructor stub
+	}
+
+	private String bib_cli_correo;
+	
+	
+	
+
+}
---
+++ biblioteca/src/biblioteca/modelo/Persona.java
@@ -0,0 +1,56 @@
+package biblioteca.modelo;
+
+import java.sql.Date;
+
+public class Persona {
+	
+	private int bib_per_id;
+	private String bib_per_nombre;
+	private String bib_per_apellido;
+	private Boolean bib_per_genero;
+	private Date bib_per_fechaNac;
+	
+	public Persona (int id, String nombre, String apellido, Boolean genero, Date fechaNac) {
+		
+		this.bib_per_id = id;
+		this.bib_per_nombre = nombre;
+		this.bib_per_apellido = apellido;
+		this.bib_per_genero = genero;
+		this.bib_per_fechaNac = fechaNac;
+		
+	}
+	
+	
+	public int getBib_per_id() {
+		return bib_per_id;
+	}
+	public void setBib_per_id(int bib_per_id) {
+		this.bib_per_id = bib_per_id;
+	}
+	
+	public String getBib_per_nombre() {
+		return bib_per_nombre;
+	}
+	public void setBib_per_nombre(String bib_per_nombre) {
+		this.bib_per_nombre = bib_per_nombre;
+	}
+	public String getBib_per_apellido() {
+		return bib_per_apellido;
+	}
+	public void setBib_per_apellido(String bib_per_apellido) {
+		this.bib_per_apellido = bib_per_apellido;
+	}
+	public Boolean getBib_per_genero() {
+		return bib_per_genero;
+	}
+	public void setBib_per_genero(Boolean bib_per_genero) {
+		this.bib_per_genero = bib_per_genero;
+	}
+	public Date getBib_per_fechaNac() {
+		return bib_per_fechaNac;
+	}
+	public void setBib_per_fechaNac(Date bib_per_fechaNac) {
+		this.bib_per_fechaNac = bib_per_fechaNac;
+	}
+
+}
