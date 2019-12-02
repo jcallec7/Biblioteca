04eba46a9b15fc2aec559dc6ef0510bca9968af7
pedro <pedro@LAPTOP-ILA641ID>
2019-12-02 01:14:30 -0500

    Solving conflicts

--- biblioteca/src/biblioteca/modelo/Cliente.java
+++ biblioteca/src/biblioteca/modelo/Cliente.java
@@ -5,7 +5,7 @@ import biblioteca.modelo.Persona;
 
 public class Cliente extends Persona {
 	
-	public Cliente(int id, String nombre, String apellido, Boolean genero, Date fechaNac) {
+	public Cliente(int id, String nombre, String apellido, String genero, String fechaNac) {
 		super(id, nombre, apellido, genero, fechaNac);
 		// TODO Auto-generated constructor stub
 	}
